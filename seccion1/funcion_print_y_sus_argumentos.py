d992cefc2ae96b42d322f37f75eba85a2e4f30e5
dj107-il <diegoballes4@gmail.com>
2026-07-27 00:04:24 -0500

    Agrega laboratorios de la seccion 1

---
+++ seccion1/funcion_print_y_sus_argumentos.py
@@ -0,0 +1,3 @@
+print("Ejercicio 1 -- Seción 1\n")
+print("Programming", "essentials", "in", sep="***", end="...")
+print("Python")
---
+++ seccion1/trabajando_funcion_print.py
@@ -0,0 +1,11 @@
+print("¡Hola, Mundo!", end="-")
+
+print("La Witsi Witsi Araña\nsubió a su telaraña.")
+print()
+print("Vino la lluvia\ny se la llevó.")
+print("Mi", "nombre", "es", "Diego", sep="-")
+print("Mi", "nombre", "es", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n\n")
+
+print("¡Hola, Mundo!")
+print("Diego Clavijo", end="😁")
